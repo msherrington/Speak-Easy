cb19a80b3a3341e98c8ed7deec7e83d26eb794c9
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-03-24 14:05:07 +0000

    review schema added

--- models/user.js
+++ models/user.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
 
+const reviewSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+});
+
+reviewSchema.methods.ownedBy = function ownedBy(user) {
+  return this.addedBy.id === user.id;
+};
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  email: {type: String, required: true },
+  email: {type: String, required: true, unique: true },
   passsword: {type: String, required: true },
   skills: [{
     language: { type: mongoose.Schema.ObjectId, ref: 'Skill' },
     level: { type: String, enum: ['Beginner', 'Intermediate', 'Advanced'] }
-  }]
+  }],
+  reviews: [ reviewSchema ]
 });
 
 module.exports = mongoose.model('User', userSchema);
