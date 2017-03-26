2688ddfe7ac2adab7ca8ae93035c92b7d480c4c5
Conor <conor.heena2@mail.dcu.ie>
2017-03-26 11:20:26 +0100

    added geolocation which centers map

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -17,12 +17,45 @@ function googleMap($window){
         center: $scope.center
       });
 
+      const currentLocationWindow = new google.maps.InfoWindow({
+        map: map
+      });
+
       const marker = new $window.google.maps.Marker({
         position: $scope.center,
         map
       });
       console.log(element);
+
+
+      //geolocation..
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          var pos = {
+            lat: parseFloat(position.coords.latitude),
+            lng: parseFloat(position.coords.longitude)
+          };
+
+          // currentLocationWindow.setPosition(pos);
+          // currentLocationWindow.setContent('Current Location');
+          marker.setPosition(pos);
+          map.setCenter(pos);
+        }, function () {
+          handleLocationError(true,  googleMap.getCenter());
+        });
+      } else {
+      // Browser doesn't support Geolocation
+        handleLocationError(false, googleMap.getCenter());
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        currentLocationWindow.setPosition(pos);
+        currentLocationWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+      }
+
+
     }
   };
+
   return directive;
 }
