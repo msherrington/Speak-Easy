f9efeb3a70340bad0b9656eefbb8483035d715ad
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-03-27 11:36:42 +0100

    fixed index display with css and adding rows and columns appropriately with map - also edit page and removed the controller from edit.html because its defined in the states

--- src/js/controller/users.js
+++ src/js/controller/users.js
@@ -102,32 +102,6 @@ function UsersProfileCtrl(User, UserReview, $stateParams, $state) {
   vm.deleteReview = deleteReview;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 UsersEditCtrl.$inject = ['User', '$stateParams', '$state'];
 function UsersEditCtrl(User, $stateParams, $state) {
   const vm = this;
