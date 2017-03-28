31ebba3f9becbec0fd90834d7c17b581c48b380d
Conor <conor.heena2@mail.dcu.ie>
2017-03-28 11:32:25 +0100

    radius stuff

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -6,33 +6,6 @@ angular.module('skillsApp')
 googleMap.$inject = ['$window', '$http'];
 function googleMap($window, $http){
   const vm = this;
-  getUser();
-
-  function getUser(){
-    $http.get('http://localhost:7000/api/users')
-    .then((response) => {
-      // console.log(response);
-      vm.all = response.data;
-      // console.log(vm.all[0].lat);
-      // console.log(vm.all[0].lng);
-      vm.u = vm.all;
-
-      const users = vm.all;
-      // console.log(users);
-      //
-      // for (var i=0; i<users.length; i++) {
-      //   console.log(users[i].lat);
-      //   console.log(users[i].lng);
-      // }
-    });
-  }
-// googleMap.$inject = ['$window', '$http'];
-// function googleMap($window, $http){
-//   const vm = this;
-
-  let userLat = 0;
-  let userLng = 0;
-  let latLng = { lat: userLat, lng: userLng };
 
   const directive = {
     restrict: 'E',
@@ -42,27 +15,25 @@ function googleMap($window, $http){
       center: '='
     },
     link($scope, element){
+
+      let userLat = 0;
+      let userLng = 0;
+      let latLng = { lat: userLat, lng: userLng };
+      let markers = [];
+
       const map = new $window.google.maps.Map(element[0], {
         zoom: 14,
         scrollwheel: false,
         center: $scope.center
       });
 
-      
-
       const marker = new $window.google.maps.Marker({
         position: $scope.center,
         map
       });
-      // console.log(element);
-
-      // const users = '???';
-      // console.log(users);
-
 
       const slider = document.getElementById('slider');
       const sliderDiv = document.getElementById('sliderAmount');
-      // let infowindow = null;
       const circle = new google.maps.Circle({
         fillColor: '#3399FF',
         fillOpacity: 0.2,
@@ -73,10 +44,6 @@ function googleMap($window, $http){
         radius: 1000
       });
 
-      // console.log(circle.radius + 'meters');
-      // console.log(circle.radius + 'meters');
-      // console.log(circle.radius * 0.000621371 + 'miles');
-
     //map circle radius function
       slider.onchange = function() {
         // console.log('Changed!');
@@ -84,11 +51,29 @@ function googleMap($window, $http){
         circle.radius = sliderDiv.innerHTML;
         //Store val of slider
         circle.setRadius(parseFloat(circle.radius));
+        console.log(markers);
         console.log(circle.radius + 'meters');
-        // console.log(circle.radius + 'miles');
-        // console.log(circle.radius * 0.000621371 + 'miles');
-      };
 
+        for(var i = 0; i < markers.length; i++){
+          // console.log('werking');
+          // console.log(markers[i].distance + 'they here');
+          if(markers[i].distance <= circle.radius){
+            console.log('its less');
+            markers[i].setMap(map);
+            console.log(markers[i].map);
+          } else{
+            console.log('its more');
+            markers[i].setMap(null);
+            console.log(markers[i].map);
+          }
+        }
+        // loop through markers
+        // check if marker.distance is less than the radius
+        // if yes, set map to map
+        // if no, set map to null
+
+
+      };
 
       //geolocation..
       if (navigator.geolocation) {
@@ -98,11 +83,10 @@ function googleMap($window, $http){
             lng: parseFloat(position.coords.longitude)
           };
 
-          // currentLocationWindow.setPosition(pos);
-          // currentLocationWindow.setContent('Current Location');
           marker.setPosition(pos);
           map.setCenter(pos);
           circle.setCenter(pos);
+          getUserLatLng(pos);
         }, function () {
           handleLocationError(true,  googleMap.getCenter());
         });
@@ -110,81 +94,60 @@ function googleMap($window, $http){
       // Browser doesn't support Geolocation
         handleLocationError(false, googleMap.getCenter());
       }
-
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        currentLocationWindow.setPosition(pos);
-        currentLocationWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+        marker.setPosition(pos);
+        marker.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
       }
 
-      addMarkers();
+      //Places User markers on the map
+      function getUserLatLng(pos) {
 
-
-      function addMarkers() {
-        // console.log('yo1');
-
-        function getUserLatLng() {
-          // console.log('yo2');
-          $http.get('http://localhost:7000/api/users')
+        $http.get('http://localhost:7000/api/users')
           .then((response) => {
             vm.all = response.data;
-            // console.log(users[0].lat);
 
             const users = vm.all;
-            // console.log(users);
-            // console.log(users[1].lat);
 
-            // for (var i=0; i<users.length; i++) {
-            //   console.log(users[i].lat);
-            //   console.log(users[i].lng);
-            // }
-
-            // console.log(users.lng);
-            // const users = 10;
-            // console.log(10);
             for (var i=0; i<users.length; i++) {
-              // console.log(`User - ${i}`);
-              // console.log(users[i].lat);
-              // console.log(users[i].lng);
               userLat = parseFloat(users[i].lat);
               userLng = parseFloat(users[i].lng);
-              addMarker(userLat, userLng);
+              addMarker(latLng, pos);
+              // findDistance(p1, p1);
             }
-
-
           });
-        }
-
-
-
-
-
-        getUserLatLng();
       }
 
-      function addMarker(userLat, userLng) {
+      function addMarker(latLng, pos) {
         // const latLng = latLng;
         var image = 'http://www.apnaplates.com/app/webroot/GSS/test/ferrari-badge-small-4.png';
         latLng = { lat: userLat, lng: userLng };
-        // console.log(latLng);
-        // console.log(latLng.userlat);
-        // console.log(latLng.userlng);
+
         const marker = new google.maps.Marker({
           position: latLng,
           map,
-          icon: image
+          icon: image,
+          distance: findDistance(new google.maps.LatLng(pos), new google.maps.LatLng(latLng))
         });
 
-        // marker.addListener('click', () => {
-        //   markerClick(marker, user);
-        // });
+        markers.push(marker);
+
+        // findDistance(new google.maps.LatLng(pos), new google.maps.LatLng(latLng));
       }
 
+      //WORKING TO FIND DISTANCE FROM A POINT//
+      //Try to set p1 as geolocation and p2 as each users latlng
+      //run the function for each user
+      // let p1 = new google.maps.LatLng({lat: 0, lng: 0});
+      // let p2 = new google.maps.LatLng({lat: 0, lng: 0});
 
+      // findDistance(p1, p2);
+      function findDistance(p1, p2){
 
-      // getUserLatLng();
+        console.log(google.maps.geometry.spherical.computeDistanceBetween(p1, p2));
+        //calculates distance between two points in km's
+        return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2)).toFixed(2);
 
-
-
+      }
     }
   };
 
