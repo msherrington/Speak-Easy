d340ea7e2da98b71067b3a165717a69b12c2deba
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-03-30 15:33:13 +0100

    took off local/host

--- src/js/controller/auth.js
+++ src/js/controller/auth.js
@@ -11,7 +11,7 @@ function RegisterCtrl($auth, $state, $http){
   getskills();
   function getskills(){
     // console.log('getskills!');
-    $http.get('http://localhost:7000/api/skills')
+    $http.get('/api/skills')
   .then((response) => {
     console.log(response);
     vm.all = response.data;
