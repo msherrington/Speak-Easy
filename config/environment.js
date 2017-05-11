33524cb34fe439aaf6f2bfe836a2ea84592c273c
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-11 11:19:01 +0100

    updated herokuapp name, and relevant link in environment.js

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,8 @@ const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'development';
 const dbURI = process.env.MONGODB_URI || `mongodb://localhost/project-3-${env}`;
 const secret = process.env.SECRET || 'I bet you never even look at this!';
-// const url = env === 'development' ? 'http://localhost:7000' : 'https://secure-oasis-40917.herokuapp.com/';
-const url = env === 'development' ? 'http://localhost:7000' : 'https://powerful-bayou-29982.herokuapp.com/';
+// const url = env === 'development' ? 'http://localhost:7000' : 'https://secure-oasis-40917.herokuapp.com/'; // Guv's link
+// const url = env === 'development' ? 'http://localhost:7000' : 'https://powerful-bayou-29982.herokuapp.com/'; //My link before renaming
+const url = env === 'development' ? 'http://localhost:7000' : 'https://speak-easy-language-exchange.herokuapp.com/'; //My link before renaming
 
 module.exports = { port, env, dbURI, secret, url };
