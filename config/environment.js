773234cb90cced99c93ae86de6ef497ff27f76ce
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-12 13:04:28 +0100

    removed old comment

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,6 @@ const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'development';
 const dbURI = process.env.MONGODB_URI || `mongodb://localhost/project-3-${env}`;
 const secret = process.env.SECRET || 'I bet you never even look at this!';
-const url = env === 'development' ? 'http://localhost:7000' : 'https://speak-easy-app.herokuapp.com/'; //My link before renaming
+const url = env === 'development' ? 'http://localhost:7000' : 'https://speak-easy-app.herokuapp.com/';
 
 module.exports = { port, env, dbURI, secret, url };
