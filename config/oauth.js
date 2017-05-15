3f2247164a39905698604966f9d97dd49e00a29b
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-15 22:14:57 +0100

    adding function to github oauth

--- config/oauth.js
+++ config/oauth.js
@@ -5,7 +5,10 @@ module.exports = {
     profileURL: 'https://api.github.com/user',
     clientId: process.env.GITHUB_SPEAKEASY_ID,
     clientSecret: process.env.GITHUB_SPEAKEASY_SECRET,
-    scope: 'user:email'
+    scope: 'user:email',
+    getLoginURL() {
+      return `${this.loginURL}?client_id=${this.clientId}&scope=${this.scope}`;
+    }
   },
   facebook: {
     loginURL: 'https://www.facebook.com/v2.9/dialog/oauth',
