f6dc1f1b3be9379db87b1427d9b19767b320dc0d
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-15 21:52:04 +0100

    changing mongo uri, trying to make github login work

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'development';
-const dbURI = process.env.MONGOLAB_RED_URI || `mongodb://localhost/project-3-${env}`;
+// const dbURI = process.env.MONGOLAB_RED_URI || `mongodb://localhost/project-3-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/project-3-${env}`;
 const secret = process.env.SECRET || 'I bet you never even look at this!';
 const url = env === 'development' ? 'http://localhost:7000' : 'https://speak-easy-app.herokuapp.com/';
 
