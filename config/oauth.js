4d5004a51e8d797f280dc27074f53934c00e4f28
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-15 15:08:10 +0100

    removing old oauth keys

--- config/oauth.js
+++ config/oauth.js
@@ -3,8 +3,6 @@ module.exports = {
     loginURL: 'https://github.com/login/oauth/authorize',
     accessTokenURL: 'https://github.com/login/oauth/access_token',
     profileURL: 'https://api.github.com/user',
-    // clientId: process.env.GITHUB_GUNIT_ID,
-    // clientSecret: process.env.GITHUB_GUNIT_SECRET,
     clientId: process.env.GITHUB_SPEAKEASY_ID,
     clientSecret: process.env.GITHUB_SPEAKEASY_SECRET,
     scope: 'user:email'
@@ -13,8 +11,6 @@ module.exports = {
     loginURL: 'https://www.facebook.com/v2.8/dialog/oauth',
     accessTokenURL: 'https://graph.facebook.com/v2.8/oauth/access_token',
     profileURL: '#',
-    // clientId: process.env.FB_GUNIT_ID,
-    // clientSecret: process.env.FB_GUNIT_SECRET,
     clientId: process.env.FB_SPEAKEASY_ID,
     clientSecret: process.env.FB_SPEAKEASY_SECRET,
     scope: 'user:email'
